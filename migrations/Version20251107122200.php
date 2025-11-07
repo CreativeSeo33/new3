0d6353db587d2c364b5bea47a72ccb3af8bae883
CreativeSeo33 <alexeyfedotof@gmail.com>
2025-11-07 15:26:51 +0300

    Добавлены новые сущности для управления хитов продаж, конфигурацией фасетных фильтров и токенами аутентификации. Обновлены связи между сущностями и уникальные ограничения в базе данных. Уточнены примечания по фасетным фильтрам и токенам аутентификации.

--- migrations/Version20251107122200.php
+++ migrations/Version20251107122200.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251106194826 extends AbstractMigration
+final class Version20251107122200 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,13 +21,17 @@ final class Version20251106194826 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE bestsellers (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, sort_order INT DEFAULT 0, INDEX idx_bestseller_product (product_id), INDEX idx_bestseller_sort_order (sort_order), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE facet_dictionary (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, attributes_json JSON DEFAULT NULL, options_json JSON DEFAULT NULL, price_min INT DEFAULT NULL, price_max INT DEFAULT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_FD_CATEGORY (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE bestsellers ADD CONSTRAINT FK_875785CA4584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE facet_dictionary ADD CONSTRAINT FK_AEEEECBB12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE bestsellers DROP FOREIGN KEY FK_875785CA4584665A');
+        $this->addSql('ALTER TABLE facet_dictionary DROP FOREIGN KEY FK_AEEEECBB12469DE2');
         $this->addSql('DROP TABLE bestsellers');
+        $this->addSql('DROP TABLE facet_dictionary');
     }
 }
